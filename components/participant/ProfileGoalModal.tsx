2951b43b0341ee971b0cba022513ae3658319d24
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2025-11-01 19:58:02 +0100

    fix

--- components/participant/ProfileGoalModal.tsx
+++ components/participant/ProfileGoalModal.tsx
@@ -3,7 +3,7 @@ import { Modal } from '../Modal';
 import { Button } from '../Button';
 import { ProfileForm, ProfileFormRef } from './ProfileGoalForm';
 import { ParticipantProfile, GenderOption, ParticipantGamificationStats, Location, NotificationSettings } from '../../types';
-import { TermsModal } from './TermsModal';
+import { TermsModal } from '../TermsModal';
 
 interface ProfileModalProps {
   isOpen: boolean;
